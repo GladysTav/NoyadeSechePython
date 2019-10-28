367975b2d7383404ad567a17c4576886987f21db
GladysTav <57091819+GladysTav@users.noreply.github.com>
2019-10-28 10:46:02 +0100

    Update Exo16.py

--- Exo16.py
+++ Exo16.py
@@ -1,23 +1,20 @@
 print("Exercice 16")
 
 #EX16: Distance entre deux mots.
-#La distance de Hamming entre deux mots de même longueur est 
-#le nombre d’endroits où les lettres sont différentes.
+#La distance de Hamming entre deux mots de mÃªme longueur est 
+#le nombre dâ€™endroits oÃ¹ les lettres sont diffÃ©rentes.
 #Par exemple : JAPON  SAVON
-#La première lettre de JAPON est différente de la première 
-#lettre de SAVON, les troisièmes aussi sont différentes. 
+#La premiÃ¨re lettre de JAPON est diffÃ©rente de la premiÃ¨re 
+#lettre de SAVON, les troisiÃ¨mes aussi sont diffÃ©rentes. 
 #La distance de Hamming entre JAPON et SAVON vaut donc 2.
-#Écris une fonction distance_hamming()qui calcule la distance de 
-#Hamming entre deux mots de même longueur.
+#Ã‰cris une fonction distance_hamming()qui calcule la distance de 
+#Hamming entre deux mots de mÃªme longueur.
 
 def dist_hamming(A, B):
     d = 0
     for a,b in zip(A,B):
-        if a != b :
-            d += 1
+        if (a != b) :
+            d = d+ 1
     return d
     
 print(dist_hamming("JAPON","SAVON"))
-    
-    
-    
